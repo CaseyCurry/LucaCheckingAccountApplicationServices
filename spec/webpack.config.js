e4761029a46d2e77deafc71fb7977a3631a274d6
CaseyCurry <casey.de.curry@gmail.com>
2017-09-09 17:57:48 -0500

    standardize webpack

--- spec/webpack.config.js
+++ spec/webpack.config.js
@@ -23,9 +23,7 @@ module.exports = () => {
           rules: [{
             test: /\.js/,
             exclude: /node_modules/,
-            use: [{
-              loader: "babel-loader"
-            }]
+            loader: "babel-loader"
           }]
         }
       });
